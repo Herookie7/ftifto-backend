c3ebfd38c3bc55dc871fca8ea4b0a56feb83b335
Herookie7 <herookie@tensi.org>
2026-01-04 20:09:00 +0530

    Update payments routes

--- src/routes/payments.routes.js
+++ src/routes/payments.routes.js
@@ -2,12 +2,21 @@ const express = require('express');
 const asyncHandler = require('express-async-handler');
 const stripeService = require('../payments/stripe.service');
 
+const razorpayService = require('../payments/razorpay.service');
+
 const router = express.Router();
 
 router.post(
   '/webhook',
   asyncHandler(async (req, res) => {
-    await stripeService.handleWebhook(req, res);
+    // Check which service the webhook belongs to based on headers or logic
+    // For now, if it has Razorpay signature, route to Razorpay service
+    if (req.headers['x-razorpay-signature']) {
+      await razorpayService.handleWebhook(req, res);
+    } else {
+      // Default to Stripe or add more logic as needed
+      await stripeService.handleWebhook(req, res);
+    }
   })
 );
 
